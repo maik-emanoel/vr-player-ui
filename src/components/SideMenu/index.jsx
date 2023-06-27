d2687e8ea19bb6f166fc4e2fcc46668211463f1b
Maik Emanoel <maikemanoel11@gmail.com>
2023-06-27 09:47:49 -0300

    feat: change icon if isPlaying is true

--- src/components/SideMenu/index.jsx
+++ src/components/SideMenu/index.jsx
@@ -1,15 +1,25 @@
-import { FolderSimpleStar, MagnifyingGlass, PersonArmsSpread, PlayCircle } from "@phosphor-icons/react";
+import {
+  FolderSimpleStar,
+  MagnifyingGlass,
+  PauseCircle,
+  PersonArmsSpread,
+  PlayCircle,
+} from "@phosphor-icons/react";
 
 // eslint-disable-next-line react/prop-types
-export default function SideMenu( {handlePlayPause} ) {
-    return (
-        <div className="flex items-center">
-            <div className="p-3 rounded-full bg-white-10 shadow-3xl backdrop-blur-lg text-white-80 flex flex-col gap-3 h-fit">
-                <PlayCircle size={40} className="icon" onClick={handlePlayPause}/>
-                <PersonArmsSpread size={40} className="icon" />
-                <FolderSimpleStar size={40} className="icon" />
-                <MagnifyingGlass size={40} className="icon" />
-            </div>
-        </div>
-    )
+export default function SideMenu({ handlePlayPause, isPlaying }) {
+  return (
+    <div className="flex items-center">
+      <div className="p-3 rounded-full bg-white-10 shadow-3xl backdrop-blur-lg text-white-80 flex flex-col gap-3 h-fit">
+        {isPlaying ? (
+          <PauseCircle size={40} className="icon" onClick={handlePlayPause} />
+        ) : (
+          <PlayCircle size={40} className="icon" onClick={handlePlayPause} />
+        )}
+        <PersonArmsSpread size={40} className="icon" />
+        <FolderSimpleStar size={40} className="icon" />
+        <MagnifyingGlass size={40} className="icon" />
+      </div>
+    </div>
+  );
 }
