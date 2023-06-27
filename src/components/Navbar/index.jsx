9a6368abe2350f51ca4901ab3810016c25f5bf93
Maik Emanoel <maikemanoel11@gmail.com>
2023-06-27 09:34:29 -0300

    feat: add function to the refresh icon

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -11,6 +11,10 @@ import {
 } from "@phosphor-icons/react";
 
 export default function Navbar() {
+  function handleRefresh() {
+    window.location.reload()
+  }
+
   return (
     <div className="py-2 px-3 rounded-full bg-white-10 shadow-3xl backdrop-blur-lg flex gap-6 text-white-80 max-w-[816px] w-full">
       <div className="flex gap-4 items-center">
@@ -29,7 +33,7 @@ export default function Navbar() {
             readOnly
           />
         </div>
-        <ArrowClockwise size={24} />
+        <ArrowClockwise size={24} className="active:scale-95 cursor-pointer" onClick={handleRefresh} />
       </div>
       <div className="flex gap-4 items-center">
         <Export size={40} className="icon" />
