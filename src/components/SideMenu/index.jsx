37ab6b1fe738d12fddbdb1b58b0fb33f29ff53d1
Maik Emanoel <maikemanoel11@gmail.com>
2023-06-27 11:32:01 -0300

    feat: add function to show & to hide volumeInput

--- src/components/SideMenu/index.jsx
+++ src/components/SideMenu/index.jsx
@@ -4,10 +4,26 @@ import {
   PauseCircle,
   PersonArmsSpread,
   PlayCircle,
+  SpeakerHigh,
 } from "@phosphor-icons/react";
+import { useState, useRef } from "react";
 
 // eslint-disable-next-line react/prop-types
 export default function SideMenu({ handlePlayPause, isPlaying }) {
+  const [isVisible, setIsVisible] = useState(false);
+  const timeoutRef = useRef(null);
+
+  function handleShowVolume() {
+    clearTimeout(timeoutRef.current);
+    setIsVisible(true);
+  }
+
+  function handleHideVolume() {
+    timeoutRef.current = setTimeout(() => {
+      setIsVisible(false);
+    }, 2000); // Tempo em milissegundos para ocultar o input range após o mouse sair
+  }
+
   return (
     <div className="flex items-center">
       <div className="p-3 rounded-full bg-white-10 shadow-3xl backdrop-blur-lg text-white-80 flex flex-col gap-3 h-fit">
@@ -16,6 +32,18 @@ export default function SideMenu({ handlePlayPause, isPlaying }) {
         ) : (
           <PlayCircle size={40} className="icon" onClick={handlePlayPause} />
         )}
+        <div
+          className="relative"
+          onMouseEnter={handleShowVolume}
+          onMouseLeave={handleHideVolume}
+        >
+          <SpeakerHigh size={40} className="icon" />
+          {isVisible && (
+            <div className="p-2 rounded-full bg-white-10 shadow-3xl backdrop-blur-lg text-white-80 flex absolute top-[50%] translate-y-[-50%] left-[150%]">
+              <input type="range" id="volumeRange" className="cursor-pointer" />
+            </div>
+          )}
+        </div>
         <PersonArmsSpread size={40} className="icon" />
         <FolderSimpleStar size={40} className="icon" />
         <MagnifyingGlass size={40} className="icon" />
