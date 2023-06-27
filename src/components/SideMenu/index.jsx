7d570a6ca6a04de152b4c16ce12f1e7b7a9ac365
Maik Emanoel <maikemanoel11@gmail.com>
2023-06-27 09:24:08 -0300

    feat: add function handlePlayPause on playCircle icon

--- src/components/SideMenu/index.jsx
+++ src/components/SideMenu/index.jsx
@@ -1,10 +1,11 @@
 import { FolderSimpleStar, MagnifyingGlass, PersonArmsSpread, PlayCircle } from "@phosphor-icons/react";
 
-export default function SideMenu() {
+// eslint-disable-next-line react/prop-types
+export default function SideMenu( {handlePlayPause} ) {
     return (
         <div className="flex items-center">
             <div className="p-3 rounded-full bg-white-10 shadow-3xl backdrop-blur-lg text-white-80 flex flex-col gap-3 h-fit">
-                <PlayCircle size={40} className="icon" />
+                <PlayCircle size={40} className="icon" onClick={handlePlayPause}/>
                 <PersonArmsSpread size={40} className="icon" />
                 <FolderSimpleStar size={40} className="icon" />
                 <MagnifyingGlass size={40} className="icon" />
